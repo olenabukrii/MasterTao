94f901aa8e4aaa004f0d530eda87d9ce08a0a74b
Elena <elena@EK.local>
2023-10-24 10:23:02 +0200

    first screen done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,26 +1,48 @@
-mobileOnlySlider("#advantages-slider", true, false, 991);
-function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
-var slider = $($slidername);
-var settings = {
-	mobileFirst: true,
-	dots: $dots,
-	arrows: $arrows,
-	responsive: [
-	{
-		breakpoint: $breakpoint,
-		settings: "unslick"
-	}
-	]
-};
+$(document).ready(function() {
 
-slider.slick(settings);
-
-$(window).on("resize", function () {
-	if ($(window).width() > $breakpoint) {
-	return;
-	}
-	if (!slider.hasClass("slick-initialized")) {
-	return slider.slick(settings);
-	}
-});
-} // Mobile Only Slider
+	$('.first-screen-slider').slick({
+		dots: true,
+		infinite: true,
+		appendArrows: '.first-screen-slider-arrows',
+		responsive: [{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+				}
+			},
+  
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		]
+	});
+  
+	mobileOnlySlider("#advantages-slider", true, false, 991);
+	function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+	var slider = $($slidername);
+	var settings = {
+		mobileFirst: true,
+		dots: $dots,
+		arrows: $arrows,
+		responsive: [
+		{
+			breakpoint: $breakpoint,
+			settings: "unslick"
+		}
+		]
+	};
+  
+	slider.slick(settings);
+  
+	$(window).on("resize", function () {
+		if ($(window).width() > $breakpoint) {
+		return;
+		}
+		if (!slider.hasClass("slick-initialized")) {
+		return slider.slick(settings);
+		}
+	});
+	} // Mobile Only Slider
+  
+  });
+  
